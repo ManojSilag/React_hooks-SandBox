0424ec9d6250f2b48b7693f32a46b0d62c63d595
ManojSilag <manojsilag@gmail.com>
2020-01-27 22:02:37 +0530

    update context api

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
+import ThemeToggle from './components/ThemeToggle';
 
 
 function App() {
   return (
     <div className="App">
+      <AuthContextProvider>
       <ThemeContextProvider>
       <Navbar/>
       <BookList/>
+      <ThemeToggle/>
       </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
