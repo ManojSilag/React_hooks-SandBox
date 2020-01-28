6c1389356ec07265b2aeae8e83af0e5ef79a4e40
ManojSilag <manojsilag@gmail.com>
2020-01-28 22:24:46 +0530

    creating multiple context api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
     // static contextType = ThemeContext;
@@ -7,24 +8,30 @@ class Navbar extends Component {
         // const { isLightTheme, light, dark} = this.context;
         // const theme = isLightTheme ? light : dark;
         return (
-
-            <ThemeContext.Consumer>{( context) =>{
-                const { isLightTheme, light, dark} = context;
-                const theme = isLightTheme ? light : dark;
-             return(
-                <nav style={{ background: theme.ui , color: theme.syantx}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>Abour</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-             )
-             
-            }}
-       
-            </ThemeContext.Consumer>
+            <AuthContext.Consumer>{(authContext) => (
+                 <ThemeContext.Consumer>{( themeContext) =>{
+                    const { isAuthenticated, toggleAuth} = authContext;
+                    const { isLightTheme, light, dark} = themeContext;
+                    const theme = isLightTheme ? light : dark;
+                 return(
+                    <nav style={{ background: theme.ui , color: theme.syantx}}>
+                    <h1>Context App</h1>
+                    <div onClick={toggleAuth}>
+                        { isAuthenticated ? 'logged In' : 'Logged out'}
+                    </div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Abour</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                 )
+                 
+                }}
+           
+                </ThemeContext.Consumer>
+            )}
+            </AuthContext.Consumer>
           );
     }
 }
